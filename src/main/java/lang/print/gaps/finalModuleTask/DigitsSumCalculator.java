a6e7ba27e45790094e239a09ed066583e633e97b
Temka <maksimukart@gmail.com>
2023-01-24 12:58:10 +0100

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int a = number/1000;
+        int b = ((number/100*100)-a*1000)/100;
+        int c = number/10-(a*100+b*10);
+        int d = number-(a*1000+b*100+c*10);
+        System.out.println(a+b+c+d);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int third = number-number/10*10;
+        int second = ((number-number/100*100)-third)/10;
+        int first = number/100;
+        System.out.println(third+""+second+""+first);
     }
 }
